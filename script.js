08901679f2fa6de4bd24cf344648bbde74b08a02
ali313As <alihuseen07400740@gmail.com>
2026-08-06 10:27:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,28 +7,32 @@ async function loadBooks() {
         booksGrid.innerHTML = "";
 
         books.forEach(book => {
-            booksGrid.innerHTML += `
-                <div class="book-card">
-                    <div class="book-cover">
-                        <div class="book-cover-icon">📚</div>
-                        <div class="book-cover-title">${book.title}</div>
+            const card = document.createElement("div");
+            card.className = "book-card";
+            card.innerHTML = `
+                <div class="book-cover">
+                    <div class="book-cover-icon">📚</div>
+                    <div class="book-cover-title">${book.title}</div>
+                </div>
+
+                <div class="book-info">
+                    <div class="book-title">${book.title}</div>
+
+                    <div class="book-author-box">
+                        <div class="author-avatar">✍️</div>
+                        <div class="book-author">${book.author}</div>
                     </div>
 
-                    <div class="book-info">
-                        <div class="book-title">${book.title}</div>
-
-                        <div class="book-author-box">
-                            <div class="author-avatar">✍️</div>
-                            <div class="book-author">${book.author}</div>
-                        </div>
-
-                        <div class="book-meta-footer">
-                            <span class="card-tag">${book.category}</span>
-                            <span class="read-btn">تصفح الآن ←</span>
-                        </div>
+                    <div class="book-meta-footer">
+                        <span class="card-tag">${book.category}</span>
+                        <span class="read-btn">تصفح الآن ←</span>
                     </div>
                 </div>
             `;
+            card.addEventListener("click", () => {
+                window.location.href = `reader.html?id=${encodeURIComponent(book.id)}`;
+            });
+            booksGrid.appendChild(card);
         });
 
         document.getElementById("count-books").textContent = books.length;
