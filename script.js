0d1c4a1b8551d8a1dc2d40367c966c7fad5909eb
ali313As <alihuseen07400740@gmail.com>
2026-08-06 09:30:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,38 @@ async function loadBooks() {
         const response = await fetch("data/books.json");
         const books = await response.json();
 
-        console.log("تم تحميل الكتب:", books);
+        const booksGrid = document.getElementById("booksGrid");
+        booksGrid.innerHTML = "";
+
+        books.forEach(book => {
+            booksGrid.innerHTML += `
+                <div class="book-card">
+                    <div class="book-cover">
+                        <div class="book-cover-icon">📚</div>
+                        <div class="book-cover-title">${book.title}</div>
+                    </div>
+
+                    <div class="book-info">
+                        <div class="book-title">${book.title}</div>
+
+                        <div class="book-author-box">
+                            <div class="author-avatar">✍️</div>
+                            <div class="book-author">${book.author}</div>
+                        </div>
+
+                        <div class="book-meta-footer">
+                            <span class="card-tag">${book.category}</span>
+                            <span class="read-btn">تصفح الآن ←</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+
+        document.getElementById("count-books").textContent = books.length;
+
     } catch (error) {
-        console.error("خطأ في تحميل الكتب:", error);
+        console.error(error);
     }
 }
 
